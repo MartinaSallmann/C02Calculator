cd454a27bbc620aba61cb8284582c0dc89baae52
MartinaSallmann <s51124@edu.campus02.at>
2021-10-08 19:59:28 +0200

    Variablen deklariert

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,6 +1,18 @@
+import java.sql.SQLOutput;
+
 public class Calculator {
 
     public static void main(String[] args) {
 
+        int a = 1;
+        System.out.println(a);
+        double var2 = 2;
+        System.out.println(var2);
+        boolean bol = true;
+        System.out.println(bol);
+        char cha = 's';
+        System.out.println(cha);
+        String st = "Martina";
+        System.out.println(st);
     }
 }
